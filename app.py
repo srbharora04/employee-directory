11062a9319eb3a8ee3b9e3e1cb50d677af54ed25
s53arora <50302318+s53arora@users.noreply.github.com>
2026-07-16 15:28:37 -0400

    Resolved stash conflict

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 print("Employee Directory - Search Feature Added")
 print ("Employee Directory - Pull Request Demo")
-print ("Login API coming soon")
-<<<<<<< Updated upstream
-=======
 print ("Salary API coming soon")
->>>>>>> Stashed changes
+
